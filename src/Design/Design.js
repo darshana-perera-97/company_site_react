f91ec9149872a8da8fa3a60bccc6c5879c7412d0
Darshana Perera <ds.perera.1997@gmail.com>
2023-08-16 20:42:56 +0530

    top bar added

---
+++ src/Design/Design.js
@@ -0,0 +1,12 @@
+import React from "react";
+import MainBanner from "./Layouts/MainBanner";
+import NavBar from "./Layouts/NavBar";
+
+export default function Design() {
+  return (
+    <div>
+      <NavBar />
+      <MainBanner />
+    </div>
+  );
+}
---
+++ src/Design/Layouts/MainBanner.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+export default function MainBanner() {
+  return (
+    <div>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6">
+            <div class="p-3">
+              <h2>Column 1</h2>
+              <p>This is content for the first column.</p>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="p-3">
+              <h2>Column 2</h2>
+              <p>This is content for the second column.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
